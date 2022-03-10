32a5dec4928624a25ba20b2c3f608eb089e937bd
Roozbeh Valavi <valavi.r@gmail.com>
2022-03-10 16:07:16 +1100

    updated maps

---
+++ R/jobs_code.R
@@ -0,0 +1,24 @@
+# read raster data; 50m landuse of 2020
+landuse <- terra::rast("../GIS_data/geotiff_clum_50m1220m/clum_50m1220m.tif")
+
+# a function to change the raster codes to a binary agriculture
+agri_codes <- function(x){
+  # codes for agriculature classes
+  code <- c(
+    # dryland and irrigated cropping
+    330, 331, 332, 333, 334, 335, 336,
+    337, 338, 430, 431, 432, 433, 434, 
+    435, 436, 437, 438,
+    # Grazing irrigated modified pastures
+    400, 420, 421, 422, 423, 424
+  )
+  y <- ifelse(x %in% code, 1, 0)
+  return(y)
+}
+
+# convert the landuse to agri/non-agri
+agriculture <- terra::app(x = landuse, 
+                          # cores = 4, 
+                          fun = agri_codes,
+                          filename = "spatial_data/agri_50m.tif",
+                          overwrite = TRUE)
--- R/modelling.R
+++ R/modelling.R
@@ -1,11 +1,12 @@
 library(tidyverse)
+library(patchwork)
 library(terra)
 library(sf)
 library(dismo)
 # library(spatstat.core)
 
 
-# read the data -----------------------------------------------------------
+# RLEM resistance data ----------------------------------------------------
 # environmental layers
 rc_proj <- rast(list.files("spatial_data/climate_proj/", full.names = TRUE))
 agri <- rast("spatial_data/agri_1km.tif") %>% 
@@ -24,7 +25,8 @@ plot(r)
 # read RLEM resistance data
 rlem_res <- read_csv("data/resist_data.csv") %>% 
   dplyr::filter(SPECIES == "Halotydeus destructor") %>% 
-  dplyr::select(SPECIES, YEAR, REGION, `CHEM.GROUP`, LONG, LAT) %>% 
+  dplyr::select(species = SPECIES, year = YEAR, region = REGION, chems = `CHEM.GROUP`, LONG, LAT) %>% 
+  filter(chems == "Pyrethroids, Pyrethrins") %>% 
   st_as_sf(coords = c("LONG", "LAT"), crs = 4326) %>%
   st_transform(crs = terra::crs(agri)) %>% 
   mutate(resistance = 1)
@@ -58,8 +60,8 @@ table(training$resistance)
 
 training[which(is.na(training$bio_04)), ]
 
-plot(r[[1]])
-plot(st_geometry(rlem_data[which(is.na(training$bio_04)), ]), add = TRUE)
+# plot(r[[1]])
+# plot(st_geometry(rlem_data[which(is.na(training$bio_04)), ]), add = TRUE)
 
 # modelling ---------------------------------------------------------------
 # calculating the case weights
@@ -82,6 +84,21 @@ brt <- gbm.step(
   site.weights = wt
 )
 
+gbm.plot(brt)
+
+# contributions
+varimp <- summary(brt)
+
+ggplot(aes(y = rel.inf, x = reorder(var, rel.inf), fill = rel.inf),
+       data = varimp) +
+  geom_bar(stat="identity") +
+  coord_flip() +
+  geom_text(aes(label = paste0(round(rel.inf, 2), "")), color = "gray5", 
+            size = 3.5, position = position_nudge(y = + 2)) +
+  viridis::scale_fill_viridis(option = "E", direction = -1) +
+  labs(y = "Relative influence (%)", x = "Variables") +
+  guides(fill = FALSE) +
+  theme_classic()
 
 # resample the maps to make predictions faster; for visualization
 # for selection pressure 'sum' might makes more sense
@@ -101,19 +118,60 @@ plot(rlem_pred, main = "RLEM resistance prediction")
 
 
 # visualisation -----------------------------------------------------------
+# read AU states
+states <- st_read("spatial_data/aus_states.gpkg") %>% 
+  st_crop(xmin = 110, ymin = -45, xmax = 155, ymax = -7)
+
 # project rasters for plotting
-reddleg_pred <- terra::project(x = rast(rlem_pred), y = "epsg:4326") %>%
+reddleg_pred <- rast(rlem_pred) %>% 
+  # terra::project(x = ., y = "epsg:4326") %>%
   as.data.frame(xy = TRUE, na.rm = TRUE) %>% 
   setNames(c("x", "y", "resistance"))
 
-# plot the resistance
-ggplot(data = reddleg_pred, aes(x = x, y = y, fill = resistance)) +
+# plot the resistance prediction
+g1 <- ggplot(data = reddleg_pred, aes(x = x, y = y, fill = resistance)) +
   geom_raster() +
   # viridis::scale_fill_viridis(option = "A", direction = 1) +
   scale_fill_gradientn(colours = terrain.colors(30, rev = TRUE)) +
-  theme_classic() +
-  coord_sf(crs = 4326) +
+  geom_sf(data = states, alpha = 0.5, col = "gray70", inherit.aes = FALSE) +
+  theme_minimal() +
+  coord_sf(crs = st_crs(rlem_data)) +
   labs(x = "Longitude", y = "Latitude", fill = "Resistance \nLikelihood")
+g1
+
+# plot the resistance points
+g2 <- ggplot(data = reddleg_pred, aes(x = x, y = y)) +
+  geom_raster(fill = "gray70") +
+  geom_sf(data = rlem_res, alpha = 0.6, col = "blue", inherit.aes = FALSE) +
+  theme_minimal() +
+  coord_sf(crs = st_crs(rlem_data)) +
+  labs(x = "Longitude", y = "Latitude")
+g2
+
+
+g1 + g2
+
+
+
+# GPA resistance data -----------------------------------------------------
+gpa_res <- read_csv("data/GPA_resistance_database_Aug162021_Roozbeh.csv") %>% 
+  filter(COUNTRY == "Australia") %>% 
+  drop_na(LATITUDE) %>% 
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326) %>%
+  st_transform(crs = terra::crs(agri))
+
+names(gpa_res)
+nrow(gpa_res)
+
+plot(gpa_res)
+
+ggplot(data = reddleg_pred, aes(x = x, y = y)) +
+  geom_raster(fill = "gray70") +
+  geom_sf(data = gpa_res, alpha = 0.6, col = "blue", inherit.aes = FALSE) +
+  theme_minimal() +
+  coord_sf(crs = st_crs(gpa_res)) +
+  labs(x = "Longitude", y = "Latitude")
+
 
 
 
